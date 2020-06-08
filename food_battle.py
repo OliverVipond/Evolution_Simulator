9369d4a1d5bbffeba692b2017963816a5ea9ed69
Todd Liebenschutz-Jones <dev@toddljones.me>
2020-06-08 21:19:00 +0100

    Change size to radius.

--- food_battle.py
+++ food_battle.py
@@ -12,6 +12,8 @@ from bokeh.driving import count
 from bokeh.io import output_notebook
 from bokeh.layouts import column, row
 
+from environment import Environment
+
 # Position: (x,y) \in [0,1]^2
 # Velocity: [dx/dt, dy/dt]
 # Energy: fraction of full energy 1
@@ -119,7 +121,7 @@ class QuadTree:
 # --------------------------------------------------------------------------------#
 
 class Blob:
-    def __init__(self, time_of_birth=0, angle=None, speed=None, position=None, energy=None, size=None):
+    def __init__(self, time_of_birth=0, angle=None, speed=None, position=None, energy=None, radius=None):
 
         self.time_of_birth = time_of_birth
 
@@ -143,19 +145,19 @@ class Blob:
         else:
             self.energy = energy
 
-        if size is None:
-            self.size = random() * 0.05
+        if radius is None:
+            self.radius = random() * 0.05
         else:
-            self.size = size
+            self.radius = radius
 
-        self.bounding_box = Rectangle(x=self.position[0] - self.size, y=self.position[1] - self.size,
-                                      width=2 * self.size, height=2 * self.size)
+        self.bounding_box = Rectangle(x=self.position[0] - self.radius, y=self.position[1] - self.radius,
+                                      width=2 * self.radius, height=2 * self.radius)
 
     def update_position(self):
         self.position += self.speed * self.get_velocity()
         self.position -= np.floor(self.position)
-        self.bounding_box = Rectangle(x=self.position[0] - self.size, y=self.position[1] - self.size,
-                                      width=2 * self.size, height=2 * self.size)
+        self.bounding_box = Rectangle(x=self.position[0] - self.radius, y=self.position[1] - self.radius,
+                                      width=2 * self.radius, height=2 * self.radius)
 
     def get_velocity(self):
         return self.speed * np.array([np.cos(self.angle), np.sin(self.angle)])
@@ -172,7 +174,7 @@ class Blob:
     def update(self):
         self.update_position()
         self.perturb_angle()
-        self.update_energy(-self.speed * self.speed * self.size * self.size * 500)
+        self.update_energy(-self.speed * self.speed * self.radius * self.radius * 500)
 
     def get_x_coordinate(self):
         return self.position[0]
@@ -184,12 +186,12 @@ class Blob:
         return Blob(time_of_birth=birth_time, speed=max(self.speed + np.random.normal(0, 0.003), 0.000001),
                     position=self.position.copy(),
                     energy=0.5,
-                    size=max(self.size + np.random.normal(0, 0.001), 0.000001)
+                    radius=max(self.radius + np.random.normal(0, 0.001), 0.000001)
                     )
 
 
 class Food:
-    def __init__(self, position=None, energy=None, size=None):
+    def __init__(self, position=None, energy=None, radius=None):
 
         if position is None:
             self.position = np.array(np.random.rand(2))
@@ -201,13 +203,13 @@ class Food:
         else:
             self.energy = energy
 
-        if size is None:
-            self.size = 0.01
+        if radius is None:
+            self.radius = 0.01
         else:
-            self.size = size
+            self.radius = radius
 
-        self.bounding_box = Rectangle(x=self.position[0] - self.size, y=self.position[1] - self.size,
-                                      width=2 * self.size, height=2 * self.size)
+        self.bounding_box = Rectangle(x=self.position[0] - self.radius, y=self.position[1] - self.radius,
+                                      width=2 * self.radius, height=2 * self.radius)
 
     # def update(self):
     #     print('update food')
@@ -221,7 +223,6 @@ class Food:
 
 class Organisms:
     def __init__(self):
-
         self.organism_list = []
 
     def update(self):
@@ -242,7 +243,6 @@ class Organisms:
 
 class Foodage:
     def __init__(self):
-
         self.food_list = []
 
     def update(self):
@@ -255,7 +255,7 @@ class Foodage:
     def add_random_foods(self, number_of_new_foods=1):
         for i in range(number_of_new_foods):
             calories = random()
-            self.add_food(Food(energy=calories * calories, size=calories / 50))
+            self.add_food(Food(energy=calories * calories, radius=calories / 50))
 
     def delete_food(self, food):
         self.food_list.remove(food)
@@ -267,14 +267,14 @@ class Foodage:
 
 blobs_source = ColumnDataSource(data=dict(x=[],
                                           y=[],
-                                          size=[],
+                                          radius=[],
                                           alpha=[]
                                           )
                                 )
 
 food_source = ColumnDataSource(data=dict(x=[],
                                          y=[],
-                                         size=[]
+                                         radius=[]
                                          )
                                )
 
@@ -284,7 +284,7 @@ stats = ColumnDataSource(data=dict(time=[],
                                    )
                          )
 
-scatter = ColumnDataSource(data=dict(pop_sizes=[],
+scatter = ColumnDataSource(data=dict(pop_radiuss=[],
                                      pop_speeds=[],
                                      time_of_birth=[]
                                      )
@@ -333,7 +333,7 @@ def make_org_data(organisms_class):
     return {
         'x': [organism.get_x_coordinate() for organism in organisms_class.organism_list],
         'y': [organism.get_y_coordinate() for organism in organisms_class.organism_list],
-        'size': [organism.size for organism in organisms_class.organism_list],
+        'radius': [organism.radius for organism in organisms_class.organism_list],
         'alpha': [max(0, min(organism.energy, 1)) for organism in organisms_class.organism_list]
     }
 
@@ -342,16 +342,16 @@ def make_food_data(foodage_class):
     return {
         'x': [food.get_x_coordinate() for food in foodage_class.food_list],
         'y': [food.get_y_coordinate() for food in foodage_class.food_list],
-        'size': [food.size for food in foodage_class.food_list]
+        'radius': [food.radius for food in foodage_class.food_list]
     }
 
 
-def consume_food(organism_quadtree, foodage_class):
+def consume_food(organism_quad_tree, foodage_class):
     for food in foodage_class.food_list:
-        close_orgs = organism_quadtree.retrieve([], food.bounding_box)
-        for organism in close_orgs:
-            if organism.size > food.size and \
-                    np.linalg.norm(food.position - organism.position) < organism.size + food.size:
+        close_organisms = organism_quad_tree.retrieve([], food.bounding_box)
+        for organism in close_organisms:
+            if organism.radius > food.radius and \
+                    np.linalg.norm(food.position - organism.position) < organism.radius + food.radius:
                 organism.eat_food(food)
                 foodage_class.delete_food(food)
                 break
@@ -369,11 +369,11 @@ def kill_reproduce(organisms_class, t):
 blobs_source.stream(make_org_data(organisms))
 food_source.stream(make_food_data(foodage))
 
-env_plot.circle('x', 'y', radius='size', alpha='alpha', source=blobs_source, fill_color='green', line_color='black')
-env_plot.circle('x', 'y', radius='size', alpha=1, source=food_source, fill_color='red', line_color='red')
+env_plot.circle('x', 'y', radius='radius', alpha='alpha', source=blobs_source, fill_color='green', line_color='black')
+env_plot.circle('x', 'y', radius='radius', alpha=1, source=food_source, fill_color='red', line_color='red')
 
-env_plot.circle('x', 'y', alpha=1, size=4, source=reachable_org, fill_color='yellow', line_color='black')
-env_plot.circle('x', 'y', alpha=1, size=4, source=tracker, fill_color='blue', line_color='blue')
+env_plot.circle('x', 'y', alpha=1, radius=4, source=reachable_org, fill_color='yellow', line_color='black')
+env_plot.circle('x', 'y', alpha=1, radius=4, source=tracker, fill_color='blue', line_color='blue')
 
 stat_plot.line('time', 'nbr_of_orgs', source=stats, line_color='green')
 stat_plot.line('time', 'nbr_of_food', source=stats, line_color='red')
@@ -383,7 +383,7 @@ stat_plot.x_range.follow_interval = 100
 color_mapper = LinearColorMapper(palette='Turbo256', low=0, high=1)
 color_bar = ColorBar(color_mapper=color_mapper, location=(0, 0))
 
-scatter_plot.circle('pop_sizes', 'pop_speeds', color={'field': 'time_of_birth', 'transform': color_mapper},
+scatter_plot.circle('pop_radii', 'pop_speeds', color={'field': 'time_of_birth', 'transform': color_mapper},
                     source=scatter)
 scatter_plot.add_layout(color_bar, 'right')
 
@@ -402,7 +402,7 @@ def update(t):
         organism_tree.insert(organism)
 
     scatter.data = {
-        'pop_sizes': [organism.size for organism in organisms.organism_list],
+        'pop_radiuss': [organism.radius for organism in organisms.organism_list],
         'pop_speeds': [organism.speed for organism in organisms.organism_list],
         'time_of_birth': [organism.time_of_birth / (t + 1) for organism in organisms.organism_list]
     }
