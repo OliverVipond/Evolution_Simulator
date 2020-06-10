9ab2c5f8b2d6bf49d26339a073c276b0232f9a2c
Todd Liebenschutz-Jones <dev@toddljones.me>
2020-06-10 14:38:27 +0100

    Remove magic numbers from Blob.

--- blob.py
+++ blob.py
@@ -8,12 +8,24 @@ class Blob:
     MAX_SPEED = 0.05
     MIN_SPEED = 0.000001
 
+    MAX_RADIUS = 0.1
+    MIN_RADIUS = 0.000001
+
+    ANGLE_PERTURBATION_RATE = 0.1
+
+    SPEED_MUTATION_RATE = 0.003
+    RADIUS_MUTATION_RATE = 0.001
+
+    ENERGY_ON_BIRTH = 0.5
+
+    MASS_TO_RADIUS_SQUARED = 1000
+
     NUMBER_OF_BLOBS = 0
 
     def __init__(self, time_of_birth=0, angle=None, speed=None, position=None, energy=None, radius=None):
 
         self.time_of_birth = time_of_birth
-        
+
         self.id = Blob.NUMBER_OF_BLOBS
         Blob.NUMBER_OF_BLOBS += 1
 
@@ -33,29 +45,37 @@ class Blob:
             self.speed = min(Blob.MAX_SPEED, max(speed, Blob.MIN_SPEED))
 
         if energy is None:
-            self.energy = 0.5
+            self.energy = Blob.ENERGY_ON_BIRTH
         else:
             self.energy = energy
 
         if radius is None:
-            self.radius = random() * 0.05
+            self.radius = Blob.MIN_RADIUS + random() * (Blob.MAX_RADIUS - Blob.MIN_RADIUS)
         else:
-            self.radius = radius
+            self.radius = min(Blob.MAX_RADIUS, max(radius, Blob.MIN_RADIUS))
 
-        self.bounding_box = Rectangle(x=self.position[0] - self.radius, y=self.position[1] - self.radius,
-                                      width=2 * self.radius, height=2 * self.radius)
+        self.bounding_box = Rectangle(
+            x=self.position[0] - self.radius,
+            y=self.position[1] - self.radius,
+            width=2 * self.radius,
+            height=2 * self.radius
+        )
 
     def update_position(self):
         self.position += self.speed * self.get_velocity()
         self.position -= np.floor(self.position)
-        self.bounding_box = Rectangle(x=self.position[0] - self.radius, y=self.position[1] - self.radius,
-                                      width=2 * self.radius, height=2 * self.radius)
+        self.bounding_box = Rectangle(
+            x=self.position[0] - self.radius,
+            y=self.position[1] - self.radius,
+            width=2 * self.radius,
+            height=2 * self.radius
+        )
 
     def get_velocity(self):
         return self.speed * np.array([np.cos(self.angle), np.sin(self.angle)])
 
     def perturb_angle(self):
-        self.angle += np.random.normal(0, 0.1)
+        self.angle += np.random.normal(0, Blob.ANGLE_PERTURBATION_RATE)
 
     def change_energy(self, delta):
         self.energy += delta
@@ -66,7 +86,10 @@ class Blob:
     def update(self):
         self.update_position()
         self.perturb_angle()
-        self.change_energy(-self.speed * self.speed * self.radius * self.radius * 500)
+        self.change_energy(-0.5 * self.speed * self.speed * self.get_mass())
+
+    def get_mass(self):
+        return self.radius * self.radius * Blob.MASS_TO_RADIUS_SQUARED
 
     def get_x_coordinate(self):
         return self.position[0]
@@ -75,11 +98,13 @@ class Blob:
         return self.position[1]
 
     def reproduce(self, birth_time):
-        return Blob(time_of_birth=birth_time, speed=self.speed + np.random.normal(0, 0.003),
-                    position=self.position.copy(),
-                    energy=0.5,
-                    radius=max(self.radius + np.random.normal(0, 0.001), 0.000001)
-                    )
+        return Blob(
+            time_of_birth=birth_time,
+            speed=self.speed + np.random.normal(0, Blob.SPEED_MUTATION_RATE),
+            position=self.position.copy(),
+            energy=Blob.ENERGY_ON_BIRTH,
+            radius=self.radius + np.random.normal(0, Blob.RADIUS_MUTATION_RATE)
+        )
 
     def curb_speed(self):
         self.speed = min(Blob.MAX_SPEED, max(self.speed, Blob.MIN_SPEED))
