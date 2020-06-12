1de336183a2ed798a6e2eb6e867942a5fb5d6a05
Oliver Vipond <oliver.vipond@gmail.com>
2020-06-12 22:26:46 +0100

    Quadtree now stores objects in leaves

--- quad_tree.py
+++ quad_tree.py
@@ -2,9 +2,23 @@ from typing import List, Any
 from helpers import str_an_array
 
 
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+
+    def rectangle_area(self):
+        return self.height * self.width
+
+    def copy(self):
+        return Rectangle(self.x, self.y, self.width, self.height)
+
+
 class QuadTree:
     nodes: List[Any]
-    MAX_OBJECTS = 10
+    MAX_OBJECTS = 5
     MAX_LEVELS = 5
 
     def __init__(self, p_bounds, p_level=1):
@@ -26,35 +40,33 @@ class QuadTree:
         y = self.bounds.y
         self.nodes = []
 
-        self.nodes.append(QuadTree(Rectangle(x + sub_width, y, sub_width, sub_height), self.level + 1))
-        self.nodes.append(QuadTree(Rectangle(x, y, sub_width, sub_height), self.level + 1))
-        self.nodes.append(QuadTree(Rectangle(x, y + sub_height, sub_width, sub_height), self.level + 1))
         self.nodes.append(QuadTree(Rectangle(x + sub_width, y + sub_height, sub_width, sub_height), self.level + 1))
+        self.nodes.append(QuadTree(Rectangle(x, y + sub_height, sub_width, sub_height), self.level + 1))
+        self.nodes.append(QuadTree(Rectangle(x, y, sub_width, sub_height), self.level + 1))
+        self.nodes.append(QuadTree(Rectangle(x + sub_width, y, sub_width, sub_height), self.level + 1))
 
-    def get_index(self, p_rect):
+    def get_index(self, p_rect: Rectangle):
         index = -1
         if not self.has_nodes():
             return index
 
         vertical_midpoint = self.bounds.x + self.bounds.width / 2
         horizontal_midpoint = self.bounds.y + self.bounds.height / 2
-        top_quadrant = (p_rect.y < horizontal_midpoint and p_rect.y + p_rect.height < horizontal_midpoint)
-        bottom_quadrant = (p_rect.y > horizontal_midpoint)
+        # bottom_quadrant = (p_rect.y < horizontal_midpoint and p_rect.y + p_rect.height < horizontal_midpoint)
+        # top_quadrant = (p_rect.y > horizontal_midpoint)
         # TODO These functions should belong to Rectangle
+        indices = []
 
-        if p_rect.x < vertical_midpoint and p_rect.x + p_rect.width < vertical_midpoint:
-            if top_quadrant:
-                index = 1
-            elif bottom_quadrant:
-                index = 2
+        if p_rect.x <= vertical_midpoint and p_rect.y <= horizontal_midpoint:
+            indices += [2]
+        if p_rect.y + p_rect.height >= horizontal_midpoint and p_rect.x <= vertical_midpoint:
+            indices += [1]
+        if p_rect.x + p_rect.width >= vertical_midpoint and p_rect.y <= horizontal_midpoint:
+            indices += [3]
+        if p_rect.x + p_rect.width >= vertical_midpoint and p_rect.y + p_rect.height >= horizontal_midpoint:
+            indices += [0]
 
-        elif p_rect.x > vertical_midpoint:
-            if top_quadrant:
-                index = 0
-            elif bottom_quadrant:
-                index = 3
-
-        return index
+        return indices
 
     def remove(self, object_to_remove):
         index = self.get_index(object_to_remove.bounding_box)
@@ -65,8 +77,8 @@ class QuadTree:
 
     def insert(self, new_object):
         if self.has_nodes():
-            index = self.get_index(new_object.bounding_box)
-            if index != -1:
+            indices = self.get_index(new_object.bounding_box)
+            for index in indices:
                 self.nodes[index].insert(new_object)
                 return
 
@@ -80,47 +92,35 @@ class QuadTree:
         if len(self.objects) > QuadTree.MAX_OBJECTS \
                 and self.level < QuadTree.MAX_LEVELS \
                 and not self.has_nodes():
-
             self.create_nodes()
             self.move_objects_to_nodes()
 
     def move_objects_to_nodes(self):
         i = 0
         while i < len(self.objects):
-            index = self.get_index(self.objects[i].bounding_box)
-            if index != -1:
-                self.nodes[index].insert(self.objects.pop(i))
+            indices = self.get_index(self.objects[i].bounding_box)
+            if indices != -1:
+                object_to_insert = self.objects.pop(i)
+                for index in indices:
+                    self.nodes[index].insert(object_to_insert)
             else:
                 i += 1
 
     def retrieve_close_objects(self, p_rect, close_objects):
-        index = self.get_index(p_rect)
-        if index != -1:
-            self.nodes[index].retrieve_close_objects(p_rect, close_objects)
-
-        close_objects += self.objects
+        indices = self.get_index(p_rect)
+        if indices != -1:
+            for index in indices:
+                self.nodes[index].retrieve_close_objects(p_rect, close_objects)
+        else:
+            close_objects += self.objects
 
     def __str__(self):
         if self.has_nodes():
             node_string = "; " \
-               + str(self.nodes[0]) + "," \
-               + self.nodes[1].__str__() + "," \
-               + self.nodes[2].__str__() + "," \
-               + self.nodes[3].__str__()
+                          + str(self.nodes[0]) + "," \
+                          + self.nodes[1].__str__() + "," \
+                          + self.nodes[2].__str__() + "," \
+                          + self.nodes[3].__str__()
         else:
             node_string = ""
         return "{" + str_an_array(self.objects) + node_string + "}"
-
-
-class Rectangle:
-    def __init__(self, x, y, width, height):
-        self.height = height
-        self.width = width
-        self.x = x
-        self.y = y
-
-    def rectangle_area(self):
-        return self.height * self.width
-
-    def copy(self):
-        return Rectangle(self.x, self.y, self.width, self.height)
